98e56a7ec74f9abbf028b3d165315c5a19a97370
andikan <ejic045440@gmail.com>
2012-08-09 18:08:02 +0800

    assignee parsing 2009 correct

--- db-parsing/assignee_parsing.rb
+++ db-parsing/assignee_parsing.rb
@@ -10,11 +10,12 @@ mypaper = mysql.db('mypaper')
 #output db
 patentproject = mysql.db('patentproject2012')
 
-patent_2009 = mypaper.query("select Patent_id, Assignee from `content_2009` limit 0,10000 ")
+patent_2009 = mypaper.query("select Patent_id, Assignee from `content_2009` ")
 
 p_more_assignee = []
 max_assignee = 0
 correct_assignee = []
+incorrect_assignee = []
 without_assignee = []
 
 patent_2009.each do |p|
@@ -22,11 +23,11 @@ patent_2009.each do |p|
   assignee = nil
   location = nil
   if p['Assignee'].nil?
-    without_assignee.push(p['Patent_id'])      
+    without_assignee.push(p['Patent_id'])
     puts "\npatent_id = #{patent_id}"
     puts "    |result = without assignee"
   else
-    assignee_str = p['Assignee'].strip().split(/([A-Z]{2}\)|\([A-Z]{2}\)\s|\s*unknown\))/)
+    assignee_str = p['Assignee'].strip().split(/([A-Z]{2}\)|\([A-Z]{2}\)|\s*unknown\))/)
     #    location_index = assignee_str.index(/\(([a-zA-Z]|\s)*,\s[A-Z]*\)/)
     #    result = /(\s*[A-Z]{2}\)|\([A-Z]{2}\))/.match(assignee_str)
     if assignee_str.count.even? #and assignee_str.count/2 > 1
@@ -59,14 +60,47 @@ patent_2009.each do |p|
       end
 
     else
+      incorrect_assignee.push(p['Patent_id'])                                                                 #
+      puts "\npatent_id = #{patent_id}"
+      puts "    |result = #{assignee_str.count}"
+      puts "          |origin = #{p['Assignee'].strip()}"
+      puts "              |str = #{assignee_str}"
 
+      assignee_str.each do |str|
+        if assignee_str.index(str).even?
+          next_str = assignee_str[assignee_str.index(str)+1]
+          if next_str.nil?
+            assignee = str.gsub(/\(/, '')
+            puts "                  |assignee = #{assignee}"
+            puts "                      |location = #{location}"
+          else
+            if next_str.match(/\([A-Z]{2}\)/)
+              assignee = str
+              location = next_str.gsub(/(\(|\))/, '')
+              puts "                  |assignee = #{assignee}"
+              puts "                      |location = #{location}"
+
+            elsif next_str.match(/\s*[A-Z]{2}\)/)
+              re_str = str.reverse
+              re_str_split = re_str.split(/\(\s{1}/)
+              if !re_str_split[1].nil?
+                assignee = re_str_split[1].reverse
+                location = (re_str_split[0].reverse + next_str).gsub(/(\(|\))/, '')
+                puts "                  |assignee = #{assignee}"
+                puts "                      |location = #{location}"
+              end
+            end
+          end
+        end
+      end
     end
-
   end
   #  patentproject.query("INSERT INTO assignee (Patent_id, Assignee, Location)
   #                       VALUES ('#{patent_id}', '#{assignee}', '#{location}')    " )
 end
 
 puts "correct assignee number = #{correct_assignee.count}"
+puts "incorrect assignee number = #{incorrect_assignee.count}"
 puts "without assignee number = #{without_assignee.count}"
+puts "check sum = #{correct_assignee.count + incorrect_assignee.count + without_assignee.count}"
 puts "total patent number = #{patent_2009.count}"
